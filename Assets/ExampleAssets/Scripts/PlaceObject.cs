a4de2c258a09b1b3c2ed590c1e832df0b9d99d12
Taha Emre Yavuz <teyavuz67@gmail.com>
2023-12-27 10:36:20 +0300

    Initial commit

---
+++ Assets/ExampleAssets/Scripts/PlaceObject.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+[RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
+public class PlaceObject : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    private ARRaycastManager _arRaycastManager;
+    private ARPlaneManager _arPlaneManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+        _arPlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouch.TouchSimulation.Enable();
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.TouchSimulation.Disable();
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
+    
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
+        if (finger.index !=0) return;
+
+        if (_arRaycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            foreach (ARRaycastHit hit in hits)
+            {
+                Pose pose = hit.pose;
+                GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
+            }
+        }
+    }
+}
